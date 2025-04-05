de62eeab2e09416791415fcf7bceac257fcadce7
MoncyDev <moncyyohannan12@gmail.com>
2025-04-05 21:17:06 +0530

    Created MainLayout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,5 @@
+import MainLayout from "@/components/MainLayout";
+
 export default function Home() {
-  return (
-    <div className="flex w-full justify-between gap-10 max-w-[1920px] mx-auto">
-      <div className="w-72 h-screen bg-[#0E0E0E]">
-        {
-          //chuchucpoookkie
-        }
-      </div>
-      <div className="w-[1200px] pt-8 pr-8 grid grid-cols-8 gap-10 h-screen">
-        <div className="col-span-6 h-full">
-          {
-            //shadow
-          }
-        </div>
-        <div className="col-span-2 h-full">
-          {
-            //matrix
-          }
-        </div>
-      </div>
-    </div>
-  );
+  return <MainLayout />;
 }
---
+++ src/components/MainLayout.tsx
@@ -0,0 +1,25 @@
+const MainLayout = () => {
+  return (
+    <div className="flex w-full justify-between gap-10 max-w-[1920px] mx-auto">
+      <div className="w-72 h-screen bg-[#0E0E0E]">
+        {
+          //chuchucpoookkie
+        }
+      </div>
+      <div className="w-[1200px] pt-8 pr-8 grid grid-cols-8 gap-10 h-screen">
+        <div className="col-span-6 h-full">
+          {
+            //shadow
+          }
+        </div>
+        <div className="col-span-2 h-full">
+          {
+            //matrix
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
