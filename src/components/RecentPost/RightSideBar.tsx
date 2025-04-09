9257e2df11de3d6e881074460ac0073394d9be3a
Dakshata <22-783@apsdhaulakuan.onmicrosoft.com>
2025-04-09 22:56:57 +0530

    recentpost

---
+++ src/components/RecentPost/RightSideBar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./RightSideBar.module.css";
+
+const RightSideBar = () => {
+  return <h1 className={styles.heading}>Recent Posts</h1>;
+};
+
+export default RightSideBar;
